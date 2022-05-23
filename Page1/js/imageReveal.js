c92c91d16f213002b6fb340a564f8933d4f646c4
Xiaohan Liao <liao.xiaohan@cca.edu>
2022-05-23 11:50:11 -0700

    floating & image reveal on p1 and p2

--- Page1/js/imageReveal.js
+++ Page1/js/imageReveal.js
@@ -1,11 +1,21 @@
-var imageId, quoteId
-for(let i=1; i<10; i++){
- imageId ="#image" + i;
- quoteId ="#quote" + i;
- /* set the quote in vinsible first*/
-$(quoteId).css("display","none");
-$(imageId).mouseover(function function_(){
-	$(quoteId).css("display", "inline-block");
+// var imageId, quoteId;
+// for(let i=1; i<10; i++){
+//  imageId =String("#image" + i);
+//  quoteId =String("#quote" + i);
+//   set the quote in vinsible first
+// $(quoteId).css("display","none");
+// $(imageId).hover(function (){
+// 	$(quoteId).css("display", "block");
+// });
+// }
+
+$("#quote1").css("display", "none");
+
+$("#image1").mouseover(function (){
+    $("#quote1").css("display", "block");
 });
-}
+
+
+
+
 
