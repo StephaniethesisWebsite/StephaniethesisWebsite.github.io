fca606721a95eab47bb10c334db1abd285105ee9
Xiaohan Liao <liao.xiaohan@cca.edu>
2022-05-25 12:40:04 -0700

    navigation functions enabled

--- js/nav.js
+++ js/nav.js
@@ -1,74 +1,57 @@
  
 
- $('#nav1').mousedown(function(){
-      $('#nav1').addClass('navitalic');
-    });
- $('#nav1').mouseup(function(){
-   $('#nav1').removeClass('navitalic');
  
-});
-$('#nav1').click(function(){
-     window.location.href='/index.html';
-     alert("click"); 
- 
-});
 
+ $('#nav1').on('mousedown', function(){
+   // alert("press");
+    $('#nav1').addClass('navitalic');
+  
+  });
    
-  $('#nav2').mousedown(function(){
-  $('#nav2').addClass('navitalic');
-    });
-
-
-    $('#nav2').mouseup(function(){
-      $('#nav2').removeClass('navitalic');
-
-    });
-
-
-$('#nav2').click(function(){
-
-     window.location.href='../Page2/2-lived-experiences.html';
+  $('#nav1').on('mouseup', function(){
  
-});
-      $('#nav3').mousedown(function(){
-      $('#nav3').addClass('navitalic');
-      // window.location.href='../Page3/3-let-s-plan-a-party.html';
-    });
-
-    $('#nav3').mouseup(function(){
-      $('#nav3').removeClass('navitalic');
-    });
-   $("#nav3").click(function (){
-
-  window.location.href='../Page3/3-let-s-plan-a-party.html';
- 
-});
-
-      $('#nav4').mousedown(function(){
-      $('#nav4').addClass('navitalic');
-      
-    });
-
-    $('#nav4').mouseup(function(){
-      $('#nav4').removeClass('navitalic');
-    });
+     $('#nav1').remove('navitalic');
    
+  });
 
- $("#nav4").click(function (){
 
-    window.location.href='../Page4/track-your-labor.html';
-});
-      $('#nav5').mousedown(function(){
-      $('#nav5').addClass('navitalic');
-      // window.location.href='../Page5/5-from-the-experts.html';
-    });
+ $('#nav2').on('mousedown', function(){
 
-    $('#nav5').mouseup(function(){
-      $('#nav5').removeClass('navitalic');
-    });
+   // alert("press");
+    $('#nav2').addClass('navitalic');
+  
+  });
    
+  $('#nav2').on('mouseup', function(){
 
- $("#nav5").click(function (){
- window.location.href='../Page5/5-from-the-experts.html';
+    $('#nav2').remove('navitalic');
+  
 
-});
+  });
+
+   $('#nav3').on('mousedown', function(){
+
+   // alert("press");
+    $('#nav3').addClass('navitalic');
+  
+  });
+   
+  $('#nav3').on('mouseup', function(){
+
+    $('#nav3').remove('navitalic');
+  
+
+  });
+   $('#nav4').on('mousedown', function(){
+
+   // alert("press");
+    $('#nav4').addClass('navitalic');
+  
+  });
+   
+  $('#nav5').on('mouseup', function(){
+
+    $('#nav5').remove('navitalic');
+  
+
+  });
