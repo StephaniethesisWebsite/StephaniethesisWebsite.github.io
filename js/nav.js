ad2756ece94b84060ce67e738dfb05a15d272fdd
Xiaohan Liao <liao.xiaohan@cca.edu>
2022-05-25 05:35:40 -0700

    navigation states

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,19 @@
+ 
+
  $('#nav1').mousedown(function(){
       $('#nav1').addClass('navitalic');
-      window.location.href='../Page1/1-cognitive-labor.html';
-    });
 
-    $('#nav1').mouseup(function(){
+    });
+ $('#nav1').mouseup(function(){
       $('#nav1').removeClass('navitalic');
     });
    
   $('#nav2').mousedown(function(){
   $('#nav2').addClass('navitalic');
  
-   window.location.href='../Page2/2-lived-experiences.html';
     });
 
+  
     $('#nav2').mouseup(function(){
       $('#nav2').removeClass('navitalic');
     });
@@ -29,7 +30,7 @@
 
       $('#nav4').mousedown(function(){
       $('#nav4').addClass('navitalic');
-        window.location.href='../Page4/track-your-labor.html';
+      
     });
 
     $('#nav4').mouseup(function(){
@@ -47,14 +48,22 @@
     });
    
 
-//    $('#nav1').click(function(){
-//    window.location.href='../Page1/1-cognitive-labor.html';
-// });
 
-//      $('#nav2').click(function(){
-//    window.location.href='../Page2/2-lived-experiences.html';
-// })
+  $("#nav1").click(function (){
+        window.location.href='../Page1/1-cognitive-labor.html';
+ 
+});
 
-//     $('#nav3').click(function(){
-//    window.location.href='../Page3/3-let-s-plan-a-party.html';
-// })
+  $("#nav2").click(function (){
+  window.location.href='../Page2/2-lived-experiences.html';
+ 
+});
+ $("#nav3").click(function (){
+  window.location.href='../Page2/2-lived-experiences.html';
+ 
+}); $("#nav4").click(function (){
+    window.location.href='../Page4/track-your-labor.html';
+ 
+}); $("#nav5").click(function (){
+ window.location.href='../Page5/5-from-the-experts.html'; 
+});
